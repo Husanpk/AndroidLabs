1d3262cead7f95e65300b118a372fe3a2c4204e7
Husanpreet Kaur <husankaur22@gmail.com>
2022-02-10 14:01:32 -0500

    lab2

--- app/src/main/java/com/cst2335/kaur0714/MainActivity.java
+++ app/src/main/java/com/cst2335/kaur0714/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cst2335.kaur0714;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.service.autofill.VisibilitySetterAction;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.w3c.dom.Text;
@@ -21,11 +24,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_main_linear);
     }
-}
+    public void display_Toast(View v){
+
+        Toast.makeText(MainActivity.this,getString(R.string.toast_message)  , Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    public void display_snack (View v) {
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch toggle = (Switch) findViewById(R.id.sw);
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                Snackbar snack;
+                if (isChecked) {
+                    snack = Snackbar.make(toggle, getString(R.string.snackbar_message), Snackbar.LENGTH_LONG);
+
+                } else {
+                    snack = Snackbar.make(toggle, getString(R.string.snackbar_message), Snackbar.LENGTH_LONG);
+
+                }
+                snack.setAction(getString(R.string.snackbar_button), click -> compoundButton.setChecked(false));
+                snack.show();
+
+            }
+        })
+        ;}}
 
 
 
